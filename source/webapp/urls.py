0f1294f663a9132ce091522081e5ddc176526b01
Janik2019 <lucky-lucky-7777@mail.ru>
2019-11-16 17:52:51 +0600

    Доделал второй этап

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, ProductView, ProductCreateView
+from .views import IndexView, ProductView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'webapp'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('products/<int:pk>/', ProductView.as_view(), name='product_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import reverse
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from webapp.models import Product, Review
 
 class IndexView(ListView):
@@ -17,3 +17,20 @@ class ProductCreateView(CreateView):
     template_name = 'product/create.html'
     fields = ('name', 'category', 'desctiption', 'photo')
     success_url = reverse_lazy('webapp:index')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'product/update.html'
+    fields = ('name', 'category', 'desctiption', 'photo')
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    template_name = 'product/delete.html'
+    success_url = reverse_lazy('webapp:index')
+    model = Product
+    context_object_name = 'product'
