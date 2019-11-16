85cd31b34fdcfd63d7a434979b867deb3987543e
Janik2019 <lucky-lucky-7777@mail.ru>
2019-11-16 15:57:44 +0600

    сделал вывод списка товаров на главной

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from webapp.models import Product, Review
 
-# Create your views here.
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
